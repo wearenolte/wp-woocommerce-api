8c8d803f667acc5d7ae1691ee5ca9454aa7f924b
raul <marreror91@gmail.com>
2016-05-31 12:00:26 -0400

    Fix CI

--- src/Utils/ErrorCodes.php
+++ src/Utils/ErrorCodes.php
@@ -1,5 +1,10 @@
 <?php namespace Lean\Woocommerce\Utils;
 
+/**
+ * Class ErrorCodes
+ *
+ * @package Lean\Woocommerce\Utils
+ */
 class ErrorCodes
 {
 	const METHOD_ERROR = 'method_not_registered';
