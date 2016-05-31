682bfa5c8f131b3aa7b4f018817c2730028d3295
raul <marreror91@gmail.com>
2016-05-31 15:58:32 -0400

    Allow new orders for guests sending shipping/billing info

--- src/Api/Order.php
+++ src/Api/Order.php
@@ -14,8 +14,8 @@ use Lean\Woocommerce\Utils\ErrorCodes;
 class Order extends AbstractEndpoint
 {
 
-	const SHIPPING_REQUIRED_FIELDS = [];
-	const BILLING_REQUIRED_FIELDS = [];
+	const SHIPPING_REQUIRED_FIELDS = [ 'first_name', 'last_name', 'address_1', 'city', 'state', 'postcode', 'country', 'email' ];
+	const BILLING_REQUIRED_FIELDS = [ 'first_name', 'last_name', 'address_1', 'city', 'state', 'postcode', 'country' ];
 	const BILLING_KEY = 'billing';
 	const SHIPPING_KEY = 'shipping';
 
@@ -80,20 +80,6 @@ class Order extends AbstractEndpoint
 					return false === $order_id || intval( $order_id ) >= 0;
 				},
 			],
-			'shipping' => [
-				'default' => false,
-				'required' => false,
-				'validate_callback' => function( $shipping ) {
-					return false === $shipping || is_array( $shipping );
-				},
-			],
-			'billing' => [
-				'default' => false,
-				'required' => false,
-				'validate_callback' => function( $billing ) {
-					return false === $billing || is_array( $billing );
-				},
-			],
 		];
 	}
 
@@ -127,8 +113,9 @@ class Order extends AbstractEndpoint
 		$order = new \WC_Order( $order_id );
 
 		do_action( 'ln_wc_after_order', $request, $cart );
+
 		// If the user is not logged in, we need to pass the billing and shipping address to the order.
-		if ( !is_user_logged_in() ) {
+		if ( ! is_user_logged_in() ) {
 			return self::update_guest_order( $request, $order );
 		}
 
@@ -138,14 +125,79 @@ class Order extends AbstractEndpoint
 		return $order;
 	}
 
+	/**
+	 * If the user is not logged in, we need the shipping and billing info in order to create the shop order.
+	 *
+	 * @param \WP_REST_Request $request Request object.
+	 * @param \WC_Order 	   $order Order.
+	 * @return \WP_Error || \WC_Order
+	 */
 	public static function update_guest_order( \WP_REST_Request $request, $order ) {
 		do_action( 'ln_wc_pre_update_guest_order', $request, $order );
 
+		$params = $request->get_body_params();
+
+		if ( ! self::check_post_address( $params ) ) {
+			return new \WP_Error(
+				ErrorCodes::BAD_REQUEST,
+				'Guest order needs billing and shipping information in POST body.',
+				[ 'status' => HttpCodes::HTTP_BAD_REQUEST ]
+			);
+		}
+
+		if ( self::fields_have_errors( $params ) ) {
+			return new \WP_Error(
+				ErrorCodes::BAD_REQUEST,
+				'Shipping and/or billing address are not completed. Please provide the minimum fields in POST body.',
+				[ 'status' => HttpCodes::HTTP_BAD_REQUEST ]
+			);
+		}
+
 		// Update order with shipping and billing information for the Guest.
+		$order->set_address( $params['shipping'], self::SHIPPING_KEY );
+		$order->set_address( $params['billing'], self::BILLING_KEY );
 
 		do_action( 'ln_wc_after_update_guest_order', $request, $order );
 
 		return $order;
+	}
+
+	/**
+	 * Check if the required parameters are on the POST body.
+	 *
+	 * @param array	$params  Post body parameters.
+	 * @return bool
+	 */
+	public static function check_post_address( $params ) {
+		if ( ! array_key_exists( self::SHIPPING_KEY, $params ) || ! array_key_exists( self::BILLING_KEY, $params ) ) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Check if we are receiving the minimum required fields.
+	 *
+	 * @param array	$params  Post body parameters.
+	 * @return int Number of errors.
+	 */
+	public static function fields_have_errors( $params ) {
+		$errors = 0;
+		$required_billing = count( self::BILLING_REQUIRED_FIELDS );
+		$required_shipping = count( self::SHIPPING_REQUIRED_FIELDS );
+
+		$array_billing = array_flip( self::BILLING_REQUIRED_FIELDS );
+		$array_shipping = array_flip( self::SHIPPING_REQUIRED_FIELDS );
+
+		if ( count( array_intersect_key( $params[ self::BILLING_KEY ], $array_billing ) ) < $required_billing ) {
+			$errors += 1;
+		}
+
+		if ( count( array_intersect_key( $params[ self::SHIPPING_KEY ], $array_shipping ) ) < $required_shipping ) {
+			$errors += 1;
+		}
+
+		return $errors;
 	}
 
 	/**
